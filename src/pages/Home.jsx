94cde8bb5a38c831ab5738d8e39f41ef3abba750
Adrianaortiz00 <a0207056@gmail.com>
2024-08-22 12:09:29 +0200

    config git ignore, sass

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,6 @@ import { API_GET_TRAVEL } from "../config/url";
 import useApi from "../service/useApi";
 
 const Home = () => {
-  const Data = useApi();
-  if (Data) {
-    console.log(Data);
-  }
   return <p>Home</p>;
 };
 
--- src/service/useApi.jsx
+++ src/service/useApi.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-const useApi = () => {
+const useApi = (url) => {
   const [data, setData] = useState(undefined);
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await axios.get("http://localhost:3001/travels");
+        const response = await axios.get(url);
         const jsonData = await response.data;
         setData(jsonData);
       } catch (error) {
@@ -13,7 +13,7 @@ const useApi = () => {
       }
     };
     getData();
-  }, []);
+  }, [url]);
   return data;
 };
 export default useApi;
