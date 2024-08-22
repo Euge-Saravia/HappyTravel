543d42d995c2f75e65c3ac9e2afa49999a86644e
Deiximar Virriel <deiximar@gmail.com>
2024-08-22 23:08:12 +0200

    Create Info button

---
+++ src/components/buttons/InfoButton.jsx
@@ -0,0 +1,9 @@
+import "./Button.scss";
+const InfoButton = () => {
+  return (
+    <button className="infoButton">
+      <img src="assets/icons/Info-icon.svg" />
+    </button>
+  );
+};
+export default InfoButton;
