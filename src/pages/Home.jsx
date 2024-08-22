e2ec0dc67f2aefb575e266d79634caacaccdbd37
Deiximar Virriel <deiximar@gmail.com>
2024-08-22 23:09:33 +0200

    Create card travel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import { API_GET_TRAVEL } from "../config/url";
 import useApi from "../service/useApi";
+import CardTravel from "../components/cards/CardTravel";
 
 const Home = () => {
-  return <p>Home</p>;
+  return <CardTravel />;
 };
 
 export default Home;
