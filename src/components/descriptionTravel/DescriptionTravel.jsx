5d9af61c5f99e2d6638423e9318e41a457e67bea
Adrianaortiz00 <a0207056@gmail.com>
2024-08-22 15:27:41 +0200

    structure description travel

---
+++ src/components/descriptionTravel/DescriptionTravel.jsx
@@ -0,0 +1,13 @@
+import "./descriptionTravel.scss"
+
+
+const DescriptionTravel = () => {
+  return (
+    <div className="description">
+      <label for="DescriptionTravel">¿Por qué quieres viajar allí? </label>
+      <input type="text" name="DescriptionTravel"/>
+    </div>
+  );
+};
+
+export default DescriptionTravel;
--- src/pages/TravelCard.jsx
+++ src/pages/TravelCard.jsx
@@ -1,7 +1,12 @@
+import DescriptionTravel from "../components/descriptionTravel/DescriptionTravel";
+
 const TravelCard = () => {
-    return (
-        <p>TravelCard</p>
-    )
-}
+  return (
+    <div>
+      <DescriptionTravel />
+      <p>TravelCard</p>
+    </div>
+  );
+};
 
 export default TravelCard;
