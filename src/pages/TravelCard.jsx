1998af8e50af2df88ebd53ce488bc7de00bd9fec
Adrianaortiz00 <a0207056@gmail.com>
2024-08-23 12:42:07 +0200

    strcuture/styles input file

--- src/pages/TravelCard.jsx
+++ src/pages/TravelCard.jsx
@@ -1,10 +1,12 @@
 import DescriptionTravel from "../components/descriptionTravel/DescriptionTravel";
 import EnterName from "../components/enterName/EnterName";
+import InputFile from "../components/inputFile/InputFile";
 
 const TravelCard = () => {
   return (
     <div>
       <EnterName />
+      <InputFile/>
       <DescriptionTravel />
       <p>TravelCard</p>
     </div>
