187bf11ee544e10f14b19511a76c4170db6549a8
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-08-23 08:58:49 +0200

    Create buttons acept and delete

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,12 @@
+import Buttons from "../components/buttons/Buttons";
+
 const SignIn = () => {
-    return (
-        <p>SignIn</p>
-    )
-}
+  return (
+    <>
+      <p>SignIn</p>
+      <Buttons />
+    </>
+  );
+};
 
 export default SignIn;
