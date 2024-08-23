abb939e2361b2282539d62400de5e26554c5d789
castellanorn <castellanorn@gmail.com>
2024-08-24 01:24:22 +0200

    add LogIn

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,7 +1,19 @@
+import Buttons from "../components/buttons/Buttons";
+import EnterName from "../components/labels/EnterName";
+import Title from "../components/labels/Title";
+import "../pages/form.scss";
+
 const LogIn = () => {
-    return (
-        <p>Login</p>
-    )
-}
+  return (
+    <div className="form oldUser">
+      <Title title="Acceso de Usuario"></Title>
+      <EnterName name="E-mail" placeholder={"Escribe tu e-mail ..."} />
+      <p>Debes escribir un e-mail</p>
+      <EnterName name="Contraseña" placeholder={"Escribe tu contraseña ..."} />
+      <br />
+      <Buttons />
+    </div>
+  );
+};
 
 export default LogIn;
