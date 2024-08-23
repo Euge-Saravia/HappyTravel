e9294c967d172261006574f6c2747fd829aa4834
Adrianaortiz00 <a0207056@gmail.com>
2024-08-23 15:11:37 +0200

    change name, sigIn

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,11 @@
 import Buttons from "../components/buttons/Buttons";
+import EnterName from "../components/labels/EnterName";
 
 const SignIn = () => {
   return (
     <>
       <p>SignIn</p>
+      <EnterName />
       <Buttons />
     </>
   );
