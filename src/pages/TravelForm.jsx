77a8f7095e6a3722b66c0ae6c2f1097624cb2e2f
Adrianaortiz00 <a0207056@gmail.com>
2024-08-24 21:33:51 +0200

    create form structure

--- src/pages/TravelForm.jsx
+++ src/pages/TravelForm.jsx
@@ -1,13 +1,25 @@
 import Title from "../components/labels/Title";
 import DescriptionTravel from "../components/labels/DescriptionTravel";
 import InputFile from "../components/labels/InputFile";
+import EnterName from "../components/labels/EnterName";
+import Buttons from "../components/buttons/Buttons";
 
 const TravelForm = () => {
   return (
-    <div>
-      <Title title={"Crear destino"} />
-      <InputFile />
-      <DescriptionTravel />
+    <div className="container">
+      <div className="border-form">
+      <Title title={"Crear destino"} className="title" width="99.5%" />
+      <div className="content">
+        <div className="column">
+          <EnterName name="Título" placeholder={"Escribe un título ..."} />
+          <p>título requerido</p>
+          <EnterName name="Ubicación" placeholder={"Escribe una ubicación ..."}/> 
+          <InputFile />
+          <Buttons className="buttons"/>
+        </div>
+        <DescriptionTravel className="description" />
+        </div>
+      </div>
     </div>
   );
 };
