ce36f9390745a8823bf2784b8db2ea35cac3adc1
Deiximar Virriel <deiximar@gmail.com>
2024-08-28 14:37:38 +0200

    Create login card component

--- src/config/url.js
+++ src/config/url.js
@@ -2,4 +2,6 @@ export const API_BASE_URL = "http://localhost:3001/";
 
 export const API_GET_TRAVEL = API_BASE_URL + "travels";
 
-export const API_POST_USER = API_BASE_URL + "users"
+export const API_POST_USER = API_BASE_URL + "register"
+
+export const API_POST_LOG_USER = API_BASE_URL + "login"
